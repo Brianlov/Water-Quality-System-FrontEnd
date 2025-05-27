6535b401df9a50f3799b9b93d24d16b8051ea5d2
Brian <brianooi1109@gmail.com>
2025-05-27 12:00:26 +0800

    w

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,13 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+test('renders home page', () => {
+  render(<App />);
+  const headerElement = screen.getByText(/AI-Enhanced Water Quality Monitoring System/i);
+  expect(headerElement).toBeInTheDocument();
+});
+test('renders sensor dashboard link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/View Sensor Dashboard/i);
+  expect(linkElement).toBeInTheDocument();
+});
